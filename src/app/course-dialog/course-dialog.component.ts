e7dbe4fdb8cbe42044cfb4af5ca0a0af9d7a8ea7
RonWieder <ron.wider@gmail.com>
2020-12-30 00:43:28 +0200

    Filter operator

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -37,9 +37,21 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     }
 
-    ngOnInit() {
+  ngOnInit() {
+    this.form.valueChanges.pipe(
+      filter(() => this.form.valid)
+    )
+      .subscribe(changes => {
+        const saveCourse$ = fromPromise(fetch(`/api/courses/${this.course.id}`, {
+          method: 'PUT',
+          body: JSON.stringify(changes),
+          headers: {
+            'content-type': 'application/json'
+          }
+        }));
 
-
+        saveCourse$.subscribe();
+    })
 
     }
 
