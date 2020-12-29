e6224211b2a479182e921f1b81f334255b3c5618
RonWieder <ron.wider@gmail.com>
2020-12-29 22:23:35 +0200

    Before Map operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,8 +26,17 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
-
-
+const http$ = new Observable(observer => {
+      fetch('/api/courses')
+        .then(res => {
+          return res.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+      .catch(err => observer.error(err))
+    })
     }
 
 
