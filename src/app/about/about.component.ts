a596eeb08b1b7db9607cac1fd7127df3cfc73580
whoseRon <whose69@gmail.com>
2020-12-29 23:11:57 +0200

    map operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { noop } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: 'about',
@@ -10,6 +13,15 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const http$ = createHttpObservable('/api/courses');
+
+    const courses$ = http$.pipe(
+      map(course => course['payload'])
+    );
+
+    courses$.subscribe(cour => console.log(cour), noop, () => console.log('completed'));
+
   }
 
 }
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,15 @@
+import { Observable } from "rxjs";
 
-
+export function createHttpObservable(url: string) {
+  return new Observable(observer => {
+      fetch('/api/courses')
+        .then(res => {
+          return res.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+      .catch(err => observer.error(err))
+    })
+}
